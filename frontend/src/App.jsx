f0a80256585b1f32abee2b8a976bb7c871c4f9d7
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-06-04 18:40:44 -0500

    rendered the MovieCard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import MovieCard from './components/MovieCard';
 
 function App()
 {
   return (
     <>
-
+      <MovieCard movie={{title: "Camden's movie", release_date: "2026"}}/>
     </>
   );
 }
